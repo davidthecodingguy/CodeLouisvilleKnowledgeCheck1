e16b4232be3c4fa3bab34754faca677ba46fae9a
David York <davidyork1995@gmail.com>
2022-10-03 07:04:11 -0400

    Moved CreateApartment and ConstructBuilding to dedicated classes

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/CreateBuilding.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/CreateBuilding.cs
@@ -6,25 +6,12 @@ using System.Threading.Tasks;
 
 namespace Software2KnowledgeCheck1
 {
-    internal class Construction
+    internal class CreateBuilding
     {
-        public List<Building> Buildings { get; } = new List<Building>();
+       public List<Building> Buildings { get; } = new List<Building>();
 
-        void CreateApartment(Apartment apartment)
-        {
-            // Get materials
-            var materialRepo = new MaterialsRepo();
-            var materialsNeeded = materialRepo.GetMaterials();
 
-            var permitRepo = new ZoningAndPermitRepo();
 
-            var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
-
-            if (buildingWasMade)
-            {
-                Buildings.Add(apartment);
-            }
-        }
         public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
         {
             if (permit && zoning)
