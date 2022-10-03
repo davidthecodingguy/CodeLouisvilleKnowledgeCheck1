5e081dc257cb6589a6181ac55fbe1dfecf941605
David York <davidyork1995@gmail.com>
2022-10-03 10:06:45 -0400

    Re-merged CreateApartment class to CreateBuilding

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/CreateBuilding.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/CreateBuilding.cs
@@ -10,7 +10,21 @@ namespace Software2KnowledgeCheck1
     {
        public List<Building> Buildings { get; } = new List<Building>();
 
+        void CreateApartment(Apartment apartment)
+        {
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
 
+            var permitRepo = new ZoningAndPermitRepo();
+
+            var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+
+            if (buildingWasMade)
+            {
+                Buildings.Add(apartment);
+            }
+        }
 
         public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
         {
